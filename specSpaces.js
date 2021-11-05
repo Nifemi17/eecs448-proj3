eb7a8b5f91876300d343b7eec116c751d0b0e26f
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-05 17:37:08 -0500

    Feat reposition player from special space

--- specSpaces.js
+++ specSpaces.js
@@ -12,7 +12,7 @@ function getRandomInt(max, min) {
 
 /**
 *	@pre boardArr is defined
-*	randomly sets the special spaces on the board
+*	@brief randomly generates special spaces on the board
 **/
 function setSpecialSpaces() {
 	let intervalSize = 10;
@@ -23,22 +23,26 @@ function setSpecialSpaces() {
 			var intervalMax = intervalMin + intervalSize;
 			var targetSpace = 0;
 			
-			//sets the lowest possible space to 4, since players won't land on any space below that.
+			//sets the lowest possible space to 3 (4 on the board), since players won't land on any space before that.
 			if (i == 0)
-				intervalMin += 4;
+				intervalMin += 3;
 			
-			console.log("special space:", j+5);
+			if (j+5 == WORMHOLE)
+				console.log("Special space: Wormhole");
+			
+			else if (j+5 == BLACK_HOLE)
+				console.log("Special space: Black Hole")
 			console.log("intervalMin:", intervalMin);
 			console.log("intervalMax:", intervalMax);
 			
 			//chooses a blank space that isn't the start or end
 			do {
 				targetSpace = getRandomInt(intervalMax, intervalMin);
-				console.log("targetSpace:", targetSpace);
+				console.log("Board space (targetSpace+1):", targetSpace + 1);
 			} while (targetSpace == 0 || targetSpace == boardArr.length-1 || boardArr[targetSpace] != 0)
 		
 			console.log('\n');
-			if (j+5 == WORMHOLE) {
+			if (j+5 == WORMHOLE && i < 2) {
 				boardArr[targetSpace] = WORMHOLE;
 			}
 			
@@ -51,10 +55,13 @@ function setSpecialSpaces() {
 	drawSpecialSpaces();
 }
 
+/**
+*	@pre special spaces has been generated
+*	@post special space graphics drawn on game board
+**/
 function drawSpecialSpaces() {
 	for (let i = 0; i < 3; i++) {
 		for (let j = 0; j < 10; j++) {
-			console.log("checking space", (i*10)+j+1, ":", boardArr[(i*10)+j]);
 			
 			context.beginPath();
 			if(boardArr[(i*10)+j] == WORMHOLE) {
@@ -84,4 +91,51 @@ function drawSpecialSpaces() {
 			context.fill();
 		}
 	}
+}
+
+/**
+*	@pre a special space has been landed on
+*	@return number of spaces player must move forward/back
+*	@param currentSpace the space number the player has landed on
+**/
+function getRepositionValue(currentSpace) {
+
+	let moveValue = getRandomInt(10,1);
+
+	let startSpace = 0;
+	let endSpace = boardArr.length - 1;
+	let moveBack = 0;
+	
+	if (boardArr[currentSpace] == BLACK_HOLE) {
+		console.log("Landed on a Black Hole!");
+		moveValue *= -1;
+		moveBack = 1;
+	}
+	
+	else
+		console.log("Landed on a Wormhole!");
+	
+	console.log("currentSpace:", currentSpace);
+	
+	do {
+		var newSpace = currentSpace + moveValue;
+		console.log("moveValue:", moveValue);	
+		console.log("newSpace:", newSpace);
+		
+		if (newSpace > endSpace)
+			return (endSpace - currentSpace);
+		
+		else if (newSpace < startSpace)
+			return (currentSpace * -1);
+		
+		if (boardArr[newSpace] != 0) {
+			if (moveBack)
+				moveValue--;
+			
+			else
+				moveValue++;
+		}
+	} while (boardArr[currentSpace + moveValue] != 0);
+	
+	return moveValue;
 }
