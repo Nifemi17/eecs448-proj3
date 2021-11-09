5acc1626254aad36175fb6785703ec5c44682cf9
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-09 15:51:05 -0600

    Fix special spaces only printing on 3 rows

--- specSpaces.js
+++ specSpaces.js
@@ -2,7 +2,8 @@ const WORMHOLE = 5; //Moves players forward by up to ten spaces
 const WORMHOLE_FILLSTYLE = 'purple';
 const BLACK_HOLE = 6; //Moves players back by up to ten spaces
 const BLACK_HOLE_FILLSTYLE = 'black';
-
+const ROW_NUM = 10; //total number of rows
+const COL_NUM = 10; //total number of columns
 /**
 *	returns a number between min (included) and max (excluded).
 **/
@@ -15,38 +16,39 @@ function getRandomInt(max, min) {
 *	@brief randomly generates special spaces on the board
 **/
 function setSpecialSpaces() {
-	let intervalSize = 10;
-	
-	for (let i = 0; i < 3; i++) {
+	for (let i = 0; i < ROW_NUM; i++) {
+		//j < 2 means 2 of each special space type per row
 		for (let j = 0; j < 2; j++) {
-			var intervalMin = i*intervalSize;
-			var intervalMax = intervalMin + intervalSize;
+			var rowMin = i*COL_NUM;
+			var rowMax = rowMin + COL_NUM;
 			var targetSpace = 0;
+			var spaceType = j + 5;
 			
 			//sets the lowest possible space to 3 (4 on the board), since players won't land on any space before that.
 			if (i == 0)
-				intervalMin += 3;
+				rowMin += 3;
 			
-			if (j+5 == WORMHOLE)
+			if (spaceType == WORMHOLE)
 				console.log("Special space: Wormhole");
 			
-			else if (j+5 == BLACK_HOLE)
+			else if (spaceType == BLACK_HOLE)
 				console.log("Special space: Black Hole")
-			console.log("intervalMin:", intervalMin);
-			console.log("intervalMax:", intervalMax);
+			console.log("rowMin:", rowMin);
+			console.log("rowMax:", rowMax);
 			
 			//chooses a blank space that isn't the start or end
 			do {
-				targetSpace = getRandomInt(intervalMax, intervalMin);
+				targetSpace = getRandomInt(rowMax, rowMin);
 				console.log("Board space (targetSpace+1):", targetSpace + 1);
-			} while (targetSpace == 0 || targetSpace == boardArr.length-1 || boardArr[targetSpace] != 0)
+			} while (targetSpace <= 0 || targetSpace >= boardArr.length-1 || boardArr[targetSpace] != 0)
 		
 			console.log('\n');
-			if (j+5 == WORMHOLE && i < 2) {
+			//prevents wormholes from spawning in the final row
+			if (spaceType == WORMHOLE && i < ROW_NUM - 1) {
 				boardArr[targetSpace] = WORMHOLE;
 			}
 			
-			else if (j+5 == BLACK_HOLE) {
+			else if (spaceType == BLACK_HOLE) {
 				boardArr[targetSpace] = BLACK_HOLE;
 			}
 		}
@@ -60,38 +62,33 @@ function setSpecialSpaces() {
 *	@post special space graphics drawn on game board
 **/
 function drawSpecialSpaces() {
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 10; j++) {
+	for (let i = 0; i < ROW_NUM; i++) {
+		for (let j = 0; j < COL_NUM; j++) {
+			let space = boardArr[(i*COL_NUM)+j];
 			
-			context.beginPath();
-			if(boardArr[(i*10)+j] == WORMHOLE) {
-				context.fillStyle = WORMHOLE_FILLSTYLE;
-			
-				if(i%2 == 0) {
-					context.arc((j*50)+12.5, 107.5-(i*40), 7.5, 0, 2*Math.PI);
-				}
-				
-				else if(i%2 == 1) {
-					context.arc(462.5-(j*50), 107.5-(i*40), 7.5, 0, 2*Math.PI);
-				}
-			}
+			if (space > 4) {
+				context.beginPath();
+				if(space == WORMHOLE) 
+					context.fillStyle = WORMHOLE_FILLSTYLE;
 		
-			else if(boardArr[(i*10)+j] == BLACK_HOLE) {
-				context.fillStyle = BLACK_HOLE_FILLSTYLE;
-			
+				else if(space == BLACK_HOLE) 
+					context.fillStyle = BLACK_HOLE_FILLSTYLE;
+				
 				if(i%2 == 0) {
-					context.arc((j*50)+12.5, 107.5-(i*40), 7.5, 0, 2*Math.PI);
+					context.arc((j*50)+12.5, 387.5-(i*40), 7.5, 0, 2*Math.PI);
 				}
 				
 				else if(i%2 == 1) {
-					context.arc(462.5-(j*50), 107.5-(i*40), 7.5, 0, 2*Math.PI);
+					context.arc(462.5-(j*50), 387.5-(i*40), 7.5, 0, 2*Math.PI);
 				}
+
+				context.closePath();
+				context.fill();
 			}
-			context.closePath();
-			context.fill();
 		}
 	}
 }
+
 
 /**
 *	@pre a special space has been landed on
