25993278a68f5715bbd16d7847bd3009ec131475
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-11-15 17:49:58 -0600

    Update: create SpecSpace class

--- specSpaces.js
+++ specSpaces.js
@@ -4,6 +4,68 @@ const BLACK_HOLE = 6; //Moves players back by up to ten spaces
 const BLACK_HOLE_FILLSTYLE = 'black';
 const ROW_NUM = 10; //total number of rows
 const COL_NUM = 10; //total number of columns
+
+var specArr = []; //SpecSpace object array to store the start and end spaces of all special spaces.
+
+
+class SpecSpace {
+	constructor (originSpace, endSpace) {
+		this.origin = originSpace; //special space's number
+		this.end = endSpace; //space player must move to when landing on this special space.
+	}
+	
+	//returns difference between the origin and end spaces
+	get spaceDiff () {
+		console.log("origin:", this.origin);
+		console.log("end:", this.end);
+		console.log ("diff val:", this.end - this.origin);
+		return this.end - this.origin;
+	}
+}
+
+function getOffset() {
+	let colNum = 10;
+	let rowOffset = getRandomInt(4,1);
+	let colOffset = getRandomInt(2, -1);
+	let totalOffset = (colNum * rowOffset) + colOffset;
+	
+	return totalOffset;
+}
+
+function setEnd(originSpace) {
+	let offset = getOffset();
+	
+	let firstBoardSpace = 0; //space number of board's start
+	let lastBoardSpace = boardArr.length - 1; //space number of board's end
+	let moveBack = 0;
+	
+	if (boardArr[originSpace] == BLACK_HOLE) {
+		offset *= -1;
+		moveBack = 1;
+	}
+	
+	do {
+		var target = originSpace + offset; //end space players will move to
+		console.log("offset value:", offset);	
+		
+		if (target > lastBoardSpace)
+			return lastBoardSpace;
+		
+		else if (target < firstBoardSpace)
+			return firstBoardSpace;
+		
+		if (boardArr[target] != 0) {
+			if (moveBack)
+				offset--;
+			
+			else
+				offset++;
+		}
+	} while (boardArr[target] != 0);
+	
+	return target;
+}
+
 /**
 *	returns a number between min (included) and max (excluded).
 **/
@@ -16,41 +78,49 @@ function getRandomInt(max, min) {
 *	@brief randomly generates special spaces on the board
 **/
 function setSpecialSpaces() {
+	let ctr = 0; //tracks how many special spaces have been made
+	
 	for (let i = 0; i < ROW_NUM; i++) {
 		//j < 2 means 2 of each special space type per row
 		for (let j = 0; j < 2; j++) {
 			var rowMin = i*COL_NUM;
 			var rowMax = rowMin + COL_NUM;
-			var targetSpace = 0;
+			var originSpace = 0;
+			var endSpace = -1;
 			var spaceType = j + 5;
 			
 			//sets the lowest possible space to 3 (4 on the board), since players won't land on any space before that.
 			if (i == 0)
 				rowMin += 3;
 			
-			if (spaceType == WORMHOLE)
-				console.log("Special space: Wormhole");
-			
-			else if (spaceType == BLACK_HOLE)
-				console.log("Special space: Black Hole")
 			console.log("rowMin:", rowMin);
 			console.log("rowMax:", rowMax);
 			
 			//chooses a blank space that isn't the start or end
 			do {
-				targetSpace = getRandomInt(rowMax, rowMin);
-				console.log("Board space (targetSpace+1):", targetSpace + 1);
-			} while (targetSpace <= 0 || targetSpace >= boardArr.length-1 || boardArr[targetSpace] != 0)
+				originSpace = getRandomInt(rowMax, rowMin);
+			} while (originSpace <= 0 || originSpace >= boardArr.length-1 || boardArr[originSpace] != 0)
 		
-			console.log('\n');
 			//prevents wormholes from spawning in the final row
 			if (spaceType == WORMHOLE && i < ROW_NUM - 1) {
-				boardArr[targetSpace] = WORMHOLE;
+				boardArr[originSpace] = WORMHOLE;
+				console.log("Special space: Wormhole");
 			}
 			
 			else if (spaceType == BLACK_HOLE) {
-				boardArr[targetSpace] = BLACK_HOLE;
+				boardArr[originSpace] = BLACK_HOLE;
+				console.log("Special space: Black Hole");
 			}
+			
+			endSpace = setEnd(originSpace);
+			
+			console.log("originSpace + 1:", originSpace + 1);
+			console.log("endSpace + 1:", endSpace + 1);
+			console.log('\n');
+			
+			specArr[ctr] = new SpecSpace(originSpace, endSpace);
+			ctr++;
+			
 		}
 	}
 	
@@ -96,43 +166,13 @@ function drawSpecialSpaces() {
 *	@param currentSpace the space number the player has landed on
 **/
 function getRepositionValue(currentSpace) {
-
-	let moveValue = getRandomInt(10,1);
-
-	let startSpace = 0;
-	let endSpace = boardArr.length - 1;
-	let moveBack = 0;
-	
-	if (boardArr[currentSpace] == BLACK_HOLE) {
-		console.log("Landed on a Black Hole!");
-		moveValue *= -1;
-		moveBack = 1;
-	}
-	
-	else
-		console.log("Landed on a Wormhole!");
-	
 	console.log("currentSpace:", currentSpace);
-	
-	do {
-		var newSpace = currentSpace + moveValue;
-		console.log("moveValue:", moveValue);	
-		console.log("newSpace:", newSpace);
-		
-		if (newSpace > endSpace)
-			return (endSpace - currentSpace);
-		
-		else if (newSpace < startSpace)
-			return (currentSpace * -1);
-		
-		if (boardArr[newSpace] != 0) {
-			if (moveBack)
-				moveValue--;
-			
-			else
-				moveValue++;
+	for (let i = 0; i < specArr.length + 1; i++) {
+		console.log("specArr[i].origin:", specArr[i].origin)
+		console.log(specArr[i].origin == currentSpace);
+		if (specArr[i].origin == currentSpace) {
+			console.log("got here:", i);
+			return specArr[i].spaceDiff;
 		}
-	} while (boardArr[currentSpace + moveValue] != 0);
-	
-	return moveValue;
+	}
 }
